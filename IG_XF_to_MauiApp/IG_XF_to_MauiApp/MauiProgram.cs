d0c9548ee5ce0ecaf351bb0ab7e6b010466d26bb
Estivenson Ortega Villar <eortegav@intergrupo.com>
2022-11-17 08:56:14 -0500

    Ejecución del comando para hacer la migración PS C:\Projects\Dev\IG_XF_to_Maui> upgrade-assistant upgrade IG_XF_to_Maui.sln

---
+++ IG_XF_to_MauiApp/IG_XF_to_MauiApp/MauiProgram.cs
@@ -0,0 +1,14 @@
+﻿
+namespace IG_XF_to_MauiApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>();
+
+		return builder.Build();
+	}
+}
