2791ae894d0d7b9ba7a8e984937bfeefc3ab9fd3
Estivenson Ortega Villar <eortegav@intergrupo.com>
2022-11-17 07:55:15 -0500

    Creación del proyecto IG_XF_to_MauiApp

---
+++ IG_XF_to_MauiApp/IG_XF_to_MauiApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IG_XF_to_MauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
